1cce60fdb2706ffc380c619b3921eee429e3cb2e
Adnan Kadrispahic <adikadrispahic@gmail.com>
2020-12-18 21:06:29 +0100

    added some emit events

--- public/app.js
+++ public/app.js
@@ -3,30 +3,34 @@ var socket = io();
 //DOM elements
 const chatForm = document.getElementById('chat-form');
 const chatMessage = document.getElementById('chat-message');
-const chatOutput = document.getElementById('chat-messages')
+const chatOutput = document.getElementById('chat-messages');
+const usernameForm = document.getElementById('username-form');
+const usernameInput = document.getElementById('username-input');
 
-//Listen for events from server and output to 
+//Listen for events from server
 socket.on('message', message => {
     console.log(message);
-    outputMessage(message);
 });
 
+socket.on('chat-message', message => {
+    var div = document.createElement('div');
+    div.innerHTML = "<p>" + message + "</p>";
+    chatOutput.append(div);
+})
+
+usernameForm.addEventListener('submit', (e) => {
+    e.preventDefault(); //Prevents page from reloading after form is submitted
+
+    const username = usernameInput.value;
+    socket.emit('new-user', username);
+})
 
 //adding event listener for when user sends chat message
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault(); //Prevents page from reloading after form is submitted
 
     const message = chatMessage.value; //getting value from input
-
     //Send message to server
     socket.emit('send-chat-message', message);
     chatMessage.value = "";
 })
-
-//Function for message output
-function outputMessage(message) {
-    const div = document.createElement('div');
-    console.log(message)
-    div.innerHTML = "<p><span>sender</span>: " + message + "</p>";
-    chatOutput.append(div);
-}
--- server.js
+++ server.js
@@ -12,15 +12,20 @@ http.listen(8080, () => {
 //Set folder for static files to be served by express
 app.use(express.static("public"));
 
+//For users on server
+const users = [];
+
 //Runs when user connects
 io.on('connection', (socket) => {
     console.log('a user connected with id: ' + socket.id);
 
+    //Broadcast to other users when new user connects
+    socket.on('new-user', username => {
+        socket.broadcast.emit('message', ("" + username + " has joined the chat"));
+    })
+
     //Welcome user with message
     socket.emit('message', "Welcome to ChatRoom");
-
-    //Broadcast to other users when new user connects
-    socket.broadcast.emit('message', "a user has joined the chat");
 
     //Send message when user leaves chat
     socket.on('disconnect', socket => {
@@ -30,7 +35,7 @@ io.on('connection', (socket) => {
     //Listen for when user sends a chat message
     socket.on('send-chat-message', message => {
         console.log(message);
-        io.emit('message', message);
+        io.emit('chat-message', message);
     })
 
 });
